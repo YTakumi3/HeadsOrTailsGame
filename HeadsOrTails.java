c89a52fa59a4aea9fc64ef65524a572dbddf8f81
YATABE Takumi <s1310174@linsv215.u-aizu.ac.jp>
2024-07-04 10:49:52 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println("Tossing a coin...");
+        int headsCount = 0;
+        int tailsCount = 0;
+
+        for (int i = 1; i <= 3; i++) {
+            if (random.nextBoolean()) {
+                System.out.println("Round " + i + ": Heads");
+                headsCount++;
+            } else {
+                System.out.println("Round " + i + ": Tails");
+                tailsCount++;
+            }
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+    }
+}
