87b102b39319bedfeb3c5223497cec0ef4bb2473
YATABE Takumi <s1310174@linsv215.u-aizu.ac.jp>
2024-07-04 10:55:29 +0900

    Victory message added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -19,5 +19,10 @@ public class HeadsOrTails {
         }
 
         System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+        if (headsCount > tailsCount) {
+            System.out.println("You won!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
